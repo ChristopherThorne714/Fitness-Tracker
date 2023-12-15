159573ad474204b2b1123b07df839641050d5068
ChristopherThorne714 <christhorne714@gmail.com>
2023-12-15 14:09:26 -0500

    created frontend and installed packages
    books model is broken?

--- backend/routes/api/books.js
+++ backend/routes/api/books.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // Load Book model
-const Book = require('../../models/Books');
+const Book = require('../../models/Book');
 
 // @route   GET api/books/test
 // @desc    Tests books route
