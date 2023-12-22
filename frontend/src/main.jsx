27629ba1578d8cd44eabe35672c9693d9f0f0385
ChristopherThorne714 <christhorne714@gmail.com>
2023-12-22 13:20:10 -0500

    updated homejsx and restructured main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import App from './App.jsx'
-import './index.css'
+import './index.css';
 
 
 // Bootstrap CSS & JS imports
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+// Old/Unused Components
+// import CreateBook from "./components/CreateBook";
+// import ShowBookList from "./components/ShowBookList";
+// import ShowBookDetails from "./components/ShowBookDetails";
+// import UpdateBookInfo from "./components/UpdateBookInfo";
+
 // Components imports and pages imports
-import CreateBook from "./components/CreateBook";
-import ShowBookList from "./components/ShowBookList";
-import ShowBookDetails from "./components/ShowBookDetails";
-import UpdateBookInfo from "./components/UpdateBookInfo";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup"
+import Signup from "./pages/Signup";
 
 // Routes
 const router = createBrowserRouter([
-  { path: "/", element: <ShowBookList /> },
-  { path: "/create-book", element: <CreateBook /> },
-  { path: "/show-book/:id", element: <ShowBookDetails /> },
-  { path: "/edit-book/:id", element: <UpdateBookInfo /> },
+  // { path: "/", element: <ShowBookList /> },
+  // { path: "/create-book", element: <CreateBook /> },
+  // { path: "/show-book/:id", element: <ShowBookDetails /> },
+  // { path: "/edit-book/:id", element: <UpdateBookInfo /> },
+  { path: "/", element: <Home />},
   { path: "/login", element: <Login /> },
   { path: "/signup", element: <Signup /> },
 ]);
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,5 +10,34 @@ import WorkoutForm from '../components/WorkoutForm'
 
 
 function Home() {
+    const [workouts, setWorkouts] = useState([]);
 
+    useEffect(() => {
+      axios
+        .get('http://localhost:5000/api/workouts')
+        .then((res) => {
+          setWorkouts(res.data);
+        })
+        .catch((err) => {
+          console.log('Error from Home');
+        });
+    }, []);
+  
+    // const workoutList =
+    //   workouts.length === 0
+    //     ? 'there is no workout record!'
+    //     : workouts.map((workout, k) => <BookCard book={book} key={k} />);
+
+    return(
+        <div className="home">
+        <div className="workouts">
+          {workouts && workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+        </div>
+        <WorkoutForm />
+      </div>
+    )
 }
+
+export default Home
