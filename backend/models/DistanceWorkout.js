1161b68e1b491ffd911a4b7ccccff33273d11138
ChristopherThorne714 <christhorne714@gmail.com>
2024-01-05 16:44:46 -0500

    moved distanceworkout model to its own file

---
+++ backend/models/DistanceWorkout.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const options = { discriminatorKey: 'kind', collection: 'workouts' };
+
+const Workout = require('./Workout');
+
+
+const DistanceWorkout = Workout.discriminator('Distance', new mongoose.Schema({
+    distance: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    laps: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+}, { timestamps: true}, options));
+
+module.export = DistanceWorkout;
--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -28,18 +28,4 @@ const Workout = mongoose.model('workout', baseWorkoutSchema);
 // duration
 // laps/distance
 
-const DistanceWorkout = Workout.discriminator('Distance', new mongoose.Schema({
-    distance: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    laps: {
-        type: Number,
-        required: true,
-        default: 0
-    }
-}, { timestamps: true}, options));
-
-
 module.exports = Workout;
