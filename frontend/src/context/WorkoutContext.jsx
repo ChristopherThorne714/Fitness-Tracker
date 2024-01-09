e1345167bad73859e1014b8821a350dc5317e487
ChristopherThorne714 <christhorne714@gmail.com>
2024-01-09 15:08:52 -0500

    added workoutsReducer to WorkoutContext

--- frontend/src/context/WorkoutContext.jsx
+++ frontend/src/context/WorkoutContext.jsx
@@ -1,13 +1,35 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 export const WorkoutsContext = createContext();
 
+export const workoutsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WORKOUTS': 
+          return {
+            workouts: action.payload
+          }
+        case 'CREATE_WORKOUT':
+          return {
+            workouts: [action.payload, ...state.workouts]
+          }
+        case 'DELETE_WORKOUT':
+          return {
+            workouts: state.workouts.filter((w) => w._id !== action.payload._id)
+          }
+        default:
+          return state
+    }
+}
 
 export const WorkoutsContextProvider = ({ children }) => {
     
+    const [state, dispatch] = useReducer(workoutsReducer, {
+        workouts: null
+    });
+
     return (
         <WorkoutsContext.Provider>
             { children }
         </WorkoutsContext.Provider>
-    )
-}
+    );
+};
