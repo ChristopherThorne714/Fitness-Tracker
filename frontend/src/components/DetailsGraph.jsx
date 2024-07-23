f001a4920559ba8d6f8d468f3d74b09413794fa3
ChristopherThorne714 <christhorne714@gmail.com>
2024-07-23 17:22:43 -0400

    started working on graph functionality
    need to think on it some more

--- frontend/src/components/DetailsGraph.jsx
+++ frontend/src/components/DetailsGraph.jsx
@@ -1,23 +1,33 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Plot from 'react-plotly.js';
 import '../App.css';
 
 function DetailsGraph() {
 
+    const workouts = useSelector((state) => state.workouts.value);
+    const dateRange = useSelector((state) => state.dateRange.value);
+    console.log(workouts);
+
     return(
         <div className='graph'>
             <Plot
+            // x axis is always going to be a range of dates
+            // y axis may change based on user input or automatically based on the type of workout selected
+            
                 data={[
                 {
-                    x: [1, 2, 3],
+                    x: dateRange,
                     y: [2, 6, 3],
                     type: 'scatter',
                     mode: 'lines+markers',
-                    marker: {color: 'red'},
+                    marker: {color: 'blue'},
+                    name: 'Load'
                 },
-                {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
                 ]}
-                layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+                layout = { {autosize: true} }
+                useResizeHandler = { true }
+                style= { {width: "100%", height: "100%"} }
             />
         </div>
     );
