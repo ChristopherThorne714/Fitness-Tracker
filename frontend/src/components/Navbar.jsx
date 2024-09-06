eb62cf692618a9dfd71c5cb608e81ec60a86c3df
ChristopherThorne714 <christhorne714@gmail.com>
2024-09-06 13:50:24 -0400

    added a logout button to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,18 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from '../redux/slices/authSlice';
+import { useDispatch } from 'react-redux';
+import { useCookies } from "react-cookie";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [cookies, removeCookie] = useCookies([]);
+
+  const onClick = (e) => {
+    dispatch(logout());
+    removeCookie('token');
+    navigate('/login');
+  }
 
   return (
     <header>
@@ -9,6 +21,9 @@ const Navbar = () => {
           <h1>Fitness Ally</h1>
         </Link>
         <nav>
+          <div>
+            <button onClick={onClick}>Log out</button>
+          </div>
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
